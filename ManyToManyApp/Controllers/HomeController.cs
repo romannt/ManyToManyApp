dcb26351302d36e6a45fe2f1ec646bb7c46ca229
Roman Tyshchenko <romannt@ukr.net>
2017-09-17 01:05:59 +0300

    Code for createing and editiong of a student was added.

--- ManyToManyApp/Controllers/HomeController.cs
+++ ManyToManyApp/Controllers/HomeController.cs
@@ -40,5 +40,58 @@ namespace ManyToManyApp.Controllers
 
             return View();
         }
+
+        public ActionResult Edit(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+                return HttpNotFound();
+            ViewBag.Courses = db.Courses.ToList();
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student, int[] selectedCourses)
+        {
+            Student newStudent = db.Students.Find(student.Id);
+            newStudent.Name = student.Name;
+            newStudent.Surname = student.Surname;
+            newStudent.Courses.Clear();
+            if (selectedCourses != null)
+            {
+                // получаем выбранные курсы
+                foreach (var c in db.Courses.Where(co => selectedCourses.Contains(co.Id)))
+                {
+                    newStudent.Courses.Add(c);
+                }
+            }
+            db.Entry(newStudent).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Create()
+        {
+            ViewBag.Courses = db.Courses.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student, int[] selectedCourses)
+        {
+            if (selectedCourses != null)
+            {
+                // получаем выбранные курсы
+                foreach (var c in db.Courses.Where(co => selectedCourses.Contains(co.Id)))
+                {
+                    student.Courses.Add(c);
+                }
+            }
+            db.Students.Add(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
